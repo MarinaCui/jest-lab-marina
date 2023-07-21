423c84bc15beb46ef1dc620400e4102bdc90e379
Hongyiming <102625542+MarinaCui@users.noreply.github.com>
2023-07-20 18:07:46 -0700

    unit test lab completed

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  // screen.debug();
+  userEvent.click(screen.getByTestId("checkbox"))
+  expect(screen.getByTestId("checkbox")).toBeChecked()
+});
+
+test('renders "World" on the screen', () => {
+  render(<App />);
+  
+  expect(screen.getByText(/world/i)).toBeInTheDocument();
 });
